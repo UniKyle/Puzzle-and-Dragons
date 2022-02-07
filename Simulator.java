a903ed7aa5a52a60c63f81e8da17197f63cd5431
Kyle Vu <83937785+UniKyle@users.noreply.github.com>
2022-02-07 13:37:13 -0800

    Create Simulator.java

---
+++ Simulator.java
@@ -0,0 +1,10 @@
+public class Simulator {
+
+	public static void main(String[] args) {
+		Board board = new Board();
+		System.out.println(board.toString2());
+		System.out.println(board.toString());
+
+	}
+
+}
